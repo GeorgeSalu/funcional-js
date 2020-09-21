f0567833f0a746197d73e50d225d0f9aec149648
George Salu <george.salu10@gmail.com>
2020-09-21 14:58:46 -0300

    basico de funções 3

--- funcoes/basico_3.js
+++ funcoes/basico_3.js
@@ -4,3 +4,15 @@ felizNatal()
 
 const saudacao = (nome) => "Fala "+nome+" !!!"
 console.log(saudacao('maria'))
+
+const somar = (...numeros) => {
+  let total = 0
+  for(let n of numeros) {
+    total += n
+  }
+  return total
+}
+
+console.log(somar(1,2,3))
+console.log(somar(1,2,3,4,5,6))
+console.log(somar(1,2,3,4,5,6,7,8,9,10))
