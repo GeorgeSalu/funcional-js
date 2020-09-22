ee050f969ae8924bda28f07642923693d40cf121
George Salu <george.salu10@gmail.com>
2020-09-22 11:38:29 -0300

    funcao map 2

--- funcoes/callback_3.js
+++ funcoes/callback_3.js
@@ -1,3 +1,7 @@
 const nums = [1,2,3,4,5]
 const dobro = n => n * 2
 console.log(nums.map(dobro))
+
+const nomes = ['ana', 'bia', 'gui', 'lia', 'rafa']
+const primeiraLetra = texto => texto[0]
+console.log(nomes.map(primeiraLetra))
