398dd3fb3a3d4c30740e2efe9bf22ed96207b713
George Salu <george.salu10@gmail.com>
2020-09-22 22:02:23 -0300

    desafio map, reduce e filter

---
+++ funcoes/desafio_2.js
@@ -0,0 +1,11 @@
+const carrinho = [
+  { nome: 'caneta', qtde: 10, preco: 7.99, fragil: true },
+  { nome: 'impressora', qtde: 1, preco: 649.50, fragil: true },
+  { nome: 'caderno', qtde: 4, preco: 27.10, fragil: false },
+  { nome: 'lapis', qtde: 3, preco: 5.82, fragil: false },
+  { nome: 'tesoura', qtde: 1, preco: 19.10, fragil: true }
+]
+
+// 1. fragil: true
+// 2. qtde 4,
+// 3. media totais
