979082e0815e2ab8da24df8a347b64ac3894ea79
George Salu <george.salu10@gmail.com>
2020-09-23 08:45:34 -0300

    desafio map, reduce e filter - resposta

--- funcoes/desafio_2.js
+++ funcoes/desafio_2.js
@@ -11,3 +11,18 @@ const carrinho = [
 // 1. fragil: true
 // 2. qtde 4,preco: 27.10 -> total
 // 3. media totais
+
+const media = carrinho
+  .filter(item => item.fragil)
+  .map(item => item.qtde * item.preco)
+  .reduce((acc, el, i, array) => {
+    const novaQtde = acc.qtde + 1
+    const novoTotal = acc.total + el
+    return {
+      qtde: novaQtde,
+      total: novoTotal,
+      media: novoTotal / novaQtde
+    }
+  }, { qtde: 0, total: 0, media: 0 })
+
+console.log(media)
