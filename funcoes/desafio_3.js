46495435b79eed45fa40f51b24589ba4e5e40656
George Salu <george.salu10@gmail.com>
2020-09-23 09:56:20 -0300

    dessafio promisse - resposta

--- funcoes/desafio_3.js
+++ funcoes/desafio_3.js
@@ -3,3 +3,19 @@
  * leitura de arquivo com promise
  *
  */
+const fs = require('fs')
+const path = require('path')
+
+
+function lerArquivo(caminho) {
+  return new Promise(resolve => {
+    fs.readFile(caminho, function(_, conteudo) {
+      resolve(conteudo.toString())
+    })
+    console.log('depois de ler')
+  })
+}
+
+const caminho = path.join(__dirname, 'dados.txt')
+lerArquivo(caminho)
+  .then(conteudo => conteudo.split('\n'))
