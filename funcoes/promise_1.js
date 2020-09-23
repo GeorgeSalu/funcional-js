ec621fe6efa293ff8fb1c38684d957f873b84863
George Salu <george.salu10@gmail.com>
2020-09-23 09:22:36 -0300

    promisse 1

--- funcoes/promise_1.js
+++ funcoes/promise_1.js
@@ -2,8 +2,18 @@ let p = new Promise(function(cumprirPromessa) {
   cumprirPromessa(['ana','bia','carlos','daniel'])
 })
 
+function primeiroElemento(array) {
+  return array[0]
+}
+
+function primeiraLetra(string) {
+  return string[0]
+}
+
+const letraMinuscula = letra => letra.toLowerCase()
+
 p
-  .then(valor => valor[0])
-  .then(primeiro => primeiro[0])
-  .then(letra => letra.toLowerCase())
+  .then(primeiroElemento)
+  .then(primeiraLetra)
+  .then(letraMinuscula)
   .then(letraMinuscula => console.log(letraMinuscula))
