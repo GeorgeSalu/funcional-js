778a47af0c82df3af186e99487a81487bd4bb1ee
George Salu <george.salu10@gmail.com>
2020-09-23 17:07:06 -0300

    async/await 2

---
+++ funcoes/async_await_2.js
@@ -0,0 +1,21 @@
+function gerarNumerosEntre(min, max, numerosProibidos) {
+  if(min> max) {
+    [max, min] = [min, max]
+  }
+
+  return new Promise((resolve, reject) => {
+    const fator = max - min + 1
+    const aleatorio = parseInt(Math.random() * fator) + min
+    if(numerosProibidos.includes(aleatorio)) {
+      reject('numero repetido!')
+    } else {
+      resolve(aleatorio)
+    }
+  })
+}
+
+function gerarMegaSena()
+
+gerarNumerosEntre(1,5, [1,2,4])
+  .then(console.log)
+  .catch(console.log)
