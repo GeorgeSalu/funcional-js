d9b61c4687b76d41ec3665561f35e79ddb46b86c
George Salu <george.salu10@gmail.com>
2020-09-22 21:56:38 -0300

    funcao reduce

---
+++ funcoes/callback_5.js
@@ -0,0 +1,18 @@
+const carrinho = [
+  { nome: 'caneta', qtde: 10, preco: 7.99 },
+  { nome: 'impressora', qtde: 0, preco: 649.50 },
+  { nome: 'caderno', qtde: 4, preco: 27.10 },
+  { nome: 'lapis', qtde: 3, preco: 5.82 },
+  { nome: 'tesoura', qtde: 1, preco: 19.10 }
+]
+
+const getTotal = item => item.qtde * item.preco
+const somar = (acc, el) => {
+  return acc + el
+}
+
+const totaGeral = carrinho
+  .map(getTotal)
+  .reduce(somar)
+
+console.log(totaGeral)
