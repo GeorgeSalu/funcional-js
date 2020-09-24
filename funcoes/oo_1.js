70f43ecf859e530e6c6d58f5c593bbdfe4a907ef
George Salu <george.salu10@gmail.com>
2020-09-23 22:02:29 -0300

    oo 1

---
+++ funcoes/oo_1.js
@@ -0,0 +1,17 @@
+function Produto(nome, preco, desc = 0.15) {
+  this.nome = nome
+  this.preco = preco
+  this.desc = desc
+
+  this.precoFinal = function() {
+    return this.preco * (1 - this.desc)
+  }
+
+}
+
+const p1 = new Produto('caneta', 5)
+const p2 = new Produto('geladeira', 15)
+
+console.log(p1)
+console.log(p2.preco)
+console.log(p2.precoFinal())
