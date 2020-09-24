9015d8713f18d470f0f1e5a9f046218356d06b19
George Salu <george.salu10@gmail.com>
2020-09-24 08:56:11 -0300

    oo 2

---
+++ funcoes/oo_2.js
@@ -0,0 +1,19 @@
+class Produto {
+
+  constructor(nome, preco, desc = 0.15) {
+    this.nome = nome
+    this.preco = preco
+    this.desc = desc
+  }
+
+  precoFinal() {
+    return this.preco * (1 - this.desc)
+  }
+}
+
+const p1 = new Produto('caneta', 5)
+const p2 = new Produto('geladeira', 15)
+
+console.log(p1.nome)
+console.log(p2.preco)
+console.log(p2.precoFinal())
