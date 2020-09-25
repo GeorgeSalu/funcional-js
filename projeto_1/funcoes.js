f62e4f550a9d3ce842901ce4d266fab475a3e800
George Salu <george.salu10@gmail.com>
2020-09-25 15:52:51 -0300

    usando promise

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -2,8 +2,16 @@ const fs = require('fs')
 const path = require('path')
 
 function lerDiretorio(caminho) {
-  let arquivos = fs.readdirSync(caminho)
-  return arquivos.map(arquivo => path.join(caminho, arquivo))
+  new new Promise((resolve, reject) => {
+    try {
+      let arquivos = fs.readdirSync(caminho)
+      return arquivos.map(arquivo => path.join(caminho, arquivo))
+      resolve(arquivos)
+    }catch(e) {
+      reject(e)
+    }
+
+  })
 }
 
 module.exports = {
