69870ef6ff81da42f66831fd493eaad240fdb630
George Salu <george.salu10@gmail.com>
2020-09-27 22:15:12 -0300

    indentação

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -2,7 +2,6 @@ const path = require('path')
 const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
-
 const simbolos = [
   '.', '?', '-', ',', '"', '', '_', '<i>',
   '</i>', '\r', '[', ']', '(', ')'
