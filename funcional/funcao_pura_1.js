81c9dc1d7a845de9c1dbdc0dd9151fdca992aa78
George Salu <george.salu10@gmail.com>
2020-09-29 09:28:33 -0300

    funcao pura 1

--- funcional/funcao_pura_1.js
+++ funcional/funcao_pura_1.js
@@ -3,3 +3,14 @@
  * de retorno e determinado apenas por seus valores
  * de entrada, sem efeitos colaterais observáveis
  */
+
+const PI = 3.14
+
+// impura - PI é um valor externo
+function areaCirc(raio) {
+  return raio * raio * PI
+}
+
+console.log(areaCirc(10))
+console.log(areaCirc(10))
+console.log(areaCirc(10))
