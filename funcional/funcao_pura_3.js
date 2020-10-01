44c8a634d532700241aa0a1b9e370c8b118a853f
George Salu <george.salu10@gmail.com>
2020-10-01 11:54:25 -0300

    funcao pura 3

---
+++ funcional/funcao_pura_3.js
@@ -0,0 +1,16 @@
+/**
+ * uma função pura é uma função em que o valor
+ * de retorno é determinado APENAS por seus valores
+ * de entrada, sem efeitos colaterais observáveis
+*/
+
+let qtdeDeExecucoes = 0
+
+// funcao pura
+function somar(a, b) {
+  qtdeDeExecucoes++
+  return a + b
+}
+
+console.log(qtdeDeExecucoes)
+console.log(somar(68, 31))
