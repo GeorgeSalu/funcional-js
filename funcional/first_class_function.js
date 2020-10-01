d4025d3272f2a4ab492d7fcd19e928d843b9be5d
George Salu <george.salu10@gmail.com>
2020-10-01 17:18:36 -0300

    first class function

--- funcional/first_class_function.js
+++ funcional/first_class_function.js
@@ -3,12 +3,15 @@
  * funcções de primeira classe quando funções nessa
  * linguagem são tratadas como qualquer outra variavel
  *
- */
+*/
 
 const x = 3
 const y = function(txt) {
   return `esse é o texto : ${txt}`
 }
 
+const z = () => console.log('xxxxx')
+
 console.log(x)
 console.log(y('ola'))
+z()
