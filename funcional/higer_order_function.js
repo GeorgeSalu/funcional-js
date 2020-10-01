25960379f61039dfc50348aad7e7188c95d754af
George Salu <george.salu10@gmail.com>
2020-10-01 12:04:52 -0300

    higer order functions

---
+++ funcional/higer_order_function.js
@@ -0,0 +1,23 @@
+/**
+ * funções que operam em outras funções,
+ * tomando-as como argumentos ou retornando-as,
+ * são chamadas de higher-order functions.
+ *
+ */
+
+function executar(fn, ...params) {
+  return fn(...params)
+}
+
+function somar(a, b, c) {
+  return a + b + c
+}
+
+function multi(a, b) {
+  return a * b
+}
+
+const r1 = executar(somar, 4,5,6)
+const r2 = executar(multi, 30, 40)
+
+console.log(r1, r2)
