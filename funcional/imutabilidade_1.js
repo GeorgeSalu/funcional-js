e64b3212d4e91cd4b2a687b117909073c437777c
George Salu <george.salu10@gmail.com>
2020-10-02 10:58:37 -0300

    imutabilidade 1

--- funcional/imutabilidade_1.js
+++ funcional/imutabilidade_1.js
@@ -1,4 +1,9 @@
-const nums = [3, 1, 7, 9, 4, 6]
-nums.sort()
+function ordenar(array) {
+  return [...array].sort()
+}
+
+const nums = Object.freeze([3, 1, 7, 9, 4, 6])
+const numsOrdenados = ordenar(nums)
 
 console.log(nums)
+console.log(numsOrdenados)
