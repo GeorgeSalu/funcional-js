72f43663488cfff6c5e82d7aa3bbdb6023d1f417
George Salu <george.salu10@gmail.com>
2020-10-02 11:29:35 -0300

    imutabilidade 2

---
+++ funcional/imutabilidade_2.js
@@ -0,0 +1,17 @@
+const nums = [4, 8, 3, 2, 9, 1, 9, 3]
+
+/* #2 - reduce
+const somar = (a, b) => a + b
+const total = nums.reduce(somar)
+console.log(total)
+*/
+
+/* #1 - dados mutaveis
+let total = 0
+
+for(let i = 0; i < nums.length; i++) {
+  total += nums[i]
+}
+
+console.log(total)
+*/
