1d5e075a559ff05e2d5571487a7a15210b0f9bdd
George Salu <george.salu10@gmail.com>
2020-10-02 14:32:34 -0300

    imutabilidade 2

--- funcional/imutabilidade_2.js
+++ funcional/imutabilidade_2.js
@@ -1,5 +1,16 @@
 const nums = [4, 8, 3, 2, 9, 1, 9, 3]
 
+// #3 - recursividade
+function somarArray(array, total = 0) {
+  if(array.length === 0) {
+    return total
+  }
+  return somarArray(array.slice(1), total + array[0])
+}
+
+const total = somarArray(nums)
+console.log(total)
+
 /* #2 - reduce
 const somar = (a, b) => a + b
 const total = nums.reduce(somar)
