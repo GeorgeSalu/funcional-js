f205e49317a3e6038af9b3b02d79f636ea440944
George Salu <george.salu10@gmail.com>
2020-10-02 16:13:31 -0300

    closure

--- funcional/closure.js
+++ funcional/closure.js
@@ -2,9 +2,9 @@
  * closure é quando uma função "lembra"
  * seu escopo lexico, mesmo quando a funcção
  * é executada fora desse escopo lexico
- */
+*/
 
- const somarXMais3 = require('./closure_escopo')
+const somarXMais3 = require('./closure_escopo')
 
- let x = 11
- console.log(somarXMais3())
+let x = 11
+console.log(somarXMais3())
