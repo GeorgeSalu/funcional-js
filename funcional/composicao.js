d2fd524d276e099f63d0b12e7e42bcda07a24b1c
George Salu <george.salu10@gmail.com>
2020-10-04 13:36:46 -0300

    composicao de funcoes #2

--- funcional/composicao.js
+++ funcional/composicao.js
@@ -20,10 +20,19 @@ function tornarLento(texto) {
   return texto.split('').join(' ')
 }
 
-const resultado = composicao(
+const exagerado = composicao(
   gritar,
   enfatizar,
   tornarLento
-)('para')
+)
 
-console.log(resultado)
+const umPoucoMenosExagerado = composicao(
+  gritar,
+  enfatizar
+)
+
+const resultado1 = exagerado('para')
+const resultado2 = umPoucoMenosExagerado('agora')
+
+console.log(resultado1)
+console.log(resultado2)
