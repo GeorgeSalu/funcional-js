5c90e4ffa24d7ec1b5c5e0bb552418c8f128725d
George Salu <george.salu10@gmail.com>
2020-10-04 11:52:49 -0300

    lay evaluation

---
+++ funcional/lazy_eval.js
@@ -0,0 +1,35 @@
+function eager(a, b) {
+
+  // procesamento mas pesado
+  const fim = Date.now() + 2500
+  while(Date.now() < fim) {}
+
+  const valor = Math.pow(a, 3)
+  return valor + b
+}
+
+function lazy(a) {
+
+  // procesamento mas pesado
+  const fim = Date.now() + 2500
+  while(Date.now() < fim) {}
+
+  const valor = Math.pow(a, 3)
+  return function(b) {
+    return valor + b
+  }
+}
+
+console.time('#1')
+console.log(eager(3, 100))
+console.log(eager(3, 100))
+console.log(eager(3, 100))
+console.timeEnd('#1')
+
+
+console.time('#1')
+const lazy3 = lazy(3)
+console.log(lazy3( 100))
+console.log(lazy3( 100))
+console.log(lazy3( 100))
+console.timeEnd('#1')
