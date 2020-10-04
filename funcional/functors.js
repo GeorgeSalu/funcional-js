6f1642521139400d2be7efa8939a8262bfd7958b
George Salu <george.salu10@gmail.com>
2020-10-04 15:18:05 -0300

    fuctors

---
+++ funcional/functors.js
@@ -0,0 +1,22 @@
+// wrapper
+
+const nums = [1,2,3,4,5,6]
+
+const novosNums = nums.map(el => el + 10)
+
+console.log(nums, novosNums)
+
+function TipoSeguro(valor) {
+  return {
+    valor,
+    map(fn) {
+      const novoValor = fn(this.valor)
+      return TipoSeguro(novoValor)
+    }
+  }
+}
+
+const resultado = TipoSeguro('esse é um texto')
+  .map(t => t.toUpperCase())
+
+console.log(resultado.valor)
