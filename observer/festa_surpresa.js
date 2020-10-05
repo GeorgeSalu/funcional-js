11e95491ed6625c84b1ac8e97920b5c5f1456d7a
George Salu <george.salu10@gmail.com>
2020-10-05 10:13:46 -0300

    padrao observer 2

--- observer/festa_surpresa.js
+++ observer/festa_surpresa.js
@@ -19,14 +19,17 @@ function namorada() {
   console.log('n: surpresa')
 }
 
+// observer
 function sindico() {
   console.log('s: monitorando barulho')
 }
 
+// subject
 function porteiro(interessados) {
   while(true) {
     const resp = await obterResposta('o namorado chegou? (s/n/q) ')
     if(resp.toLowerCase() === 's') {
+      // os observadores são notificados
       (interessados || []).forEach(obs => obs())
     } else if(resp.toLowerCase() === 'q') {
       break
@@ -34,4 +37,7 @@ function porteiro(interessados) {
   }
 }
 
+// chamada da função que registra dois observadores
+// os observadores são: [namorada, sindico]
+// o subject é o porteiro
 porteiro([namorada, sindico])
