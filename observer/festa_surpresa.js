956edd3036b77c48e47dcea7b7566d3f9bd15cca
George Salu <george.salu10@gmail.com>
2020-10-05 10:02:21 -0300

    padrao observer 2

--- observer/festa_surpresa.js
+++ observer/festa_surpresa.js
@@ -13,5 +13,29 @@ function obterResposta(pergunta) {
   })
 }
 
-obterResposta('esse é um teste')
-  .then(console.log)
+function namorada() {
+  setTimeout(() => {
+    console.log('n: apagar as luzes')
+    console.log('n: pedir silencio')
+    console.log('n: surpresa')
+  }, 2000)
+}
+
+function sindico() {
+  setTimeout(() => {
+    console.log('s: monitorando barulho')
+  }, 1000)
+}
+
+function porteiro(interessados) {
+  while(true) {
+    const resp = await obterResposta('o namorado chegou? (s/n/q) ')
+    if(resp.toLowerCase() === 's') {
+      (interessados || []).forEach(obs => obs())
+    } else if(resp.toLowerCase() === 'q') {
+      break
+    }
+  }
+}
+
+porteiro([namorada, sindico])
