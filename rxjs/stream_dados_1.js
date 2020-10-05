f4f30453c9539dee5a62e854b2446918d76a605c
George Salu <george.salu10@gmail.com>
2020-10-05 11:54:10 -0300

    stream de dados 1

--- rxjs/stream_dados_1.js
+++ rxjs/stream_dados_1.js
@@ -1,20 +1,40 @@
-function gerarNumeros(fn) {
+
+function gerarNumeros() {
   return {
-    iniciar(fn) {
-      let num = 0
-      setInterval(() => {
-        fn(num++)
-      }, 2000)
-    }
+      iniciar(fn, intervalo = 1000) {
+          let num = 0
+          const i = setInterval(() => {
+              fn(num++)
+          }, intervalo)
+
+          return {
+              parar() {
+                  clearInterval(i)
+              }
+          }
+      }
   }
 }
 
 const temp1 = gerarNumeros()
-temp1.iniciar(numero => {
-  console.log(`#1: ${numero * 2}`)
-})
+const exec11 = temp1.iniciar(numero => {
+  console.log(`#1.1: ${numero * 2}`)
+}, 1000)
+
+const exec12 = temp1.iniciar(numero => {
+  console.log(`#1.2: ${numero * 2}`)
+}, 500)
 
 const temp2 = gerarNumeros()
-temp2.iniciar(a => {
+const exec2 = temp2.iniciar(a => {
   console.log(`#2: ${a + 100}`)
-})
+}, 2000)
+
+setTimeout(() => {
+  exec11.parar()
+  exec2.parar()
+}, 10000)
+
+setTimeout(() => {
+  exec12.parar()
+}, 12000)
