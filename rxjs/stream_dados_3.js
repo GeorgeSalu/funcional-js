918febdfa9d0a501314b5462e91dc68515961144
George Salu <george.salu10@gmail.com>
2020-10-08 10:22:33 -0300

    stream de dados 3

--- rxjs/stream_dados_3.js
+++ rxjs/stream_dados_3.js
@@ -2,6 +2,8 @@ const {interval} = require('rxjs')
 
 const gerarNumeros = interval(500)
 
-gerarNumeros.subscribe(num => {
+const sub1 = gerarNumeros.subscribe(num => {
   console.log(Math.pow(2, num))
 })
+
+setTimeout(() => sub1.unsubscribe(), 8000)
