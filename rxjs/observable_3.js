2f91f555f0e23403d87f983fc580647f889d7159
George Salu <george.salu10@gmail.com>
2020-10-10 15:15:30 -0300

    oservable - desafio resposta

--- rxjs/observable_3.js
+++ rxjs/observable_3.js
@@ -4,7 +4,21 @@
  * entre min e max com Observable!
 */
 
+const {Observable, noop} = require('rxjs')
+
+function entre(min, max) {
+  if(min > max) [min, max] = [max, min]
+  return new Observable(subscriber => {
+    for(let i = min; i <= max;i++) {
+      subscriber.next(i)
+    }
+    subscriber.complete()
+  })
+}
+
 entre(4, 10)
   .subscribe(
-    num => console.log(`num = ${num}`)
+    num => console.log(`num = ${num}`),
+    noop,
+    () => console.log('fim')
   )
