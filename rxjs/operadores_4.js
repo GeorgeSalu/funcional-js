47a944d0e67a4a7496e5c3eb5bd05d507f999e3d
George Salu <george.salu10@gmail.com>
2020-10-13 20:47:20 -0300

    operadores 4

--- rxjs/operadores_4.js
+++ rxjs/operadores_4.js
@@ -1,9 +1,30 @@
 const {from, Observable} = require('rxjs')
 
 function primeiro() {
+  return function(source) {
+    return Observable.create(subscriber => {
+      source.subscribe({
+        next(v) {
+          subscriber.next(1)
+        }
+      })
+    })
+  }
+}
 
+function ultimo() {
+  return function(source) {
+    return Observable.create(subscriber => {
+      source.subscribe({
+        next(v) {
+          subscriber.next(v + 2000)
+        }
+      })
+    })
+  }
 }
 
 from([1,2,3,4,5])
-  .pipe()
+  //.pipe(primeiro())
+  .pipe(ultimo())
   .subscribe(console.log)
