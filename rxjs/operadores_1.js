091621e1437c00862f629e342b27d2eeb70fd4fa
George Salu <george.salu10@gmail.com>
2020-10-13 13:49:03 -0300

    operadores 1

--- rxjs/operadores_1.js
+++ rxjs/operadores_1.js
@@ -1,8 +1,13 @@
-/**
- * Os dois tipos...
- * 1. operadores de criação
- * 2. operadores de encadeamento (pipeable op.)
- */
+// Os dois tipos...
 
+ //1. operadores de criação
 const { of } = require('rxjs')
+
+// 2. operadores de encadeamento (pipeable op.)
 const { last } = require('rxjs/operators')
+
+of(1,2, 'ana', false, 'ultimo')
+  .pipe(last())
+  .subscribe(function(valor) {
+    console.log(`o valor gerado foi ${valor}`)
+  })
