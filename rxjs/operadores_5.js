8b072cc0f8f20fe6968cd4fd349d61bcbf77c228
George Salu <george.salu10@gmail.com>
2020-10-16 14:55:54 -0300

    desafio operadores 2 - resposta

--- rxjs/operadores_5.js
+++ rxjs/operadores_5.js
@@ -1,7 +1,23 @@
 const { of, Observable } = require('rxjs')
 
-function terminadoCom(textoFinal) {
-  // implementar
+function terminadoCom(parteFinal) {
+  return function(fonte) {
+    return Observable.create(subscriber => {
+      fonte.subscribe({
+        next(texto) {
+          if(texto.endsWith(parteFinal)) {
+            subscriber.next(texto)
+          }
+        },
+        error(e) {
+          subscriber.error(e)
+        },
+        complete() {
+          subscriber.complete()
+        }
+      })
+    })
+  }
 }
 
 of('ana silva', 'maria silva', 'pedro rocha')
