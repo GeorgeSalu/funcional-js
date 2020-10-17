729b8aa6402b0dc166599e397aa3f051e2ba50f0
George Salu <george.salu10@gmail.com>
2020-10-16 23:30:44 -0300

    desafio subscription - resposta

--- rxjs/subscription.js
+++ rxjs/subscription.js
@@ -1,2 +1,14 @@
 // procurar um operador de criação na documentação do rxjs capaz
 // de esperar 3000 e gerar numeros de 500 em 500
+
+const { timer } = require('rxjs')
+
+const obs = timer(3000, 500)
+
+const sub1 = obs.subscribe(num => {
+  console.log(`#1 Gerou o numero ${num}`)
+})
+
+setTimeout(() => {
+  sub1.unsubscribe()
+}, 10000)
