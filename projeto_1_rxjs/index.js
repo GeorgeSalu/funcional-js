9d474fa412d2c11b6b8124558942e459802045ef
George Salu <george.salu10@gmail.com>
2020-10-16 23:35:35 -0300

    refatorando projeto para usar rxjs

---
+++ projeto_1_rxjs/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const fn = require('./funcoes')
+
+const caminho = path.join(__dirname, '..', 'dados', 'legendas')
+const simbolos = [
+  '.', '?', '-', ',', '"', '', '_', '<i>',
+  '</i>', '\r', '[', ']', '(', ')'
+]
+
+/* usando promisse */
+fn.lerDiretorio(caminho)
+  .then(fn.elementosTerminadosCom('.srt'))
+  .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+  .then(fn.mesclarConteudos)
+  .then(fn.separarTextoPor('\n'))
+  .then(fn.removerElementoSeVazio)
+  .then(fn.removerElementoSeIncluir('-->'))
+  .then(fn.removerElementoSeApenasNumero)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarConteudos)
+  .then(fn.separarTextoPor(' '))
+  .then(fn.removerElementoSeVazio)
+  .then(fn.removerElementoSeApenasNumero)
+  .then(fn.agruparElementos)
+  .then(fn.ordenarPorAtributoNumerico('qtde'))
+  .then(console.log)
