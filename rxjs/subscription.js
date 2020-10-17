1c3a5548b2ab559898b60c352e54e3d40cdec1c9
George Salu <george.salu10@gmail.com>
2020-10-16 22:23:05 -0300

    desafio subscription

--- rxjs/subscription.js
+++ rxjs/subscription.js
@@ -1,2 +1,2 @@
-// procurarr um operador de criação na documentação do rxjs capaz
-// de esperar 3000 e gerar nunmeros de 500 em 500
+// procurar um operador de criação na documentação do rxjs capaz
+// de esperar 3000 e gerar numeros de 500 em 500
